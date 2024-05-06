2d6b4fc899a49f9a2ffe2f735e978c8e9d7e309d
quinlan-lewis <quinonis27@gmail.com>
2024-05-06 10:52:15 -0600

    Updates to animation

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function updateSize() {
+    newPositionValue = document.querySelector(".profile").offsetHeight + "px";
+    document.documentElement.style.setProperty("--bg-position-x", newPositionValue);
+}
+
+window.addEventListener('load', function (event) {
+    updateSize();
+});
+
+window.addEventListener('resize', function (event) {
+    updateSize();
+});
